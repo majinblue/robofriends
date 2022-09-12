3dd975aa268fb33859eb15e02f47cece5e3aecb6
Essan'Utep Blue <eblue.codes@gmail.com>
2022-09-12 10:13:05 -0700

    Created the logic to render the robots array from the robots.js file.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import Card from './Card.js'
+import CardList from './CardList';
+import { robots } from './robots';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div>
-      <Card />
-      <Card />
+      <CardList robots={robots}/>
     </div>
     
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
