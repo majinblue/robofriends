4cb01f81d3239e0b28365607dee2182bd9e3d2e3
Essan'Utep Blue <eblue.codes@gmail.com>
2022-09-13 13:07:48 -0700

    Fixed GH pages link.

--- src/containers/App.js
+++ src/containers/App.js
@@ -43,9 +43,9 @@ class App extends Component {
           <h1 className='f1'>RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange}/>
           <Scroll>
-            {/* <ErrorBoundry> */}
+            <ErrorBoundry>
               <CardList robots={filteredRobots} />
-            {/* </ErrorBoundry> */}
+            </ErrorBoundry>
           </Scroll>
         </div>
       )
