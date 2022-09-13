9a15f6a6e5d9f52211c36df574dac76ab6780fb9
Essan'Utep Blue <eblue.codes@gmail.com>
2022-09-13 12:06:21 -0700

    Updates

---
+++ precache-manifest.f9b6d0325fce581720ae6842d1ad74ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e7d9d2e646665ccca58",
+    "url": "/majinblue/robofriends/static/css/main.622ba77a.chunk.css"
+  },
+  {
+    "revision": "1e7d9d2e646665ccca58",
+    "url": "/majinblue/robofriends/static/js/main.1e7d9d2e.chunk.js"
+  },
+  {
+    "revision": "672d79b3d25199064c96",
+    "url": "/majinblue/robofriends/static/css/1.0f6f3d35.chunk.css"
+  },
+  {
+    "revision": "672d79b3d25199064c96",
+    "url": "/majinblue/robofriends/static/js/1.672d79b3.chunk.js"
+  },
+  {
+    "revision": "5d9da9eb7ef43f6f1d85",
+    "url": "/majinblue/robofriends/static/js/runtime~main.5d9da9eb.js"
+  },
+  {
+    "revision": "020a72f0cc51c0d604578f101957f436",
+    "url": "/majinblue/robofriends/static/media/Roseritta.020a72f0.woff"
+  },
+  {
+    "revision": "bdeb93652aa252aebd75ac22f1148b82",
+    "url": "/majinblue/robofriends/index.html"
+  }
+];
